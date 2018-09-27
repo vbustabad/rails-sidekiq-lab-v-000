f40f60f37ed95968b1c166262e9da6ba9047bc08
Veronica Bustabad <vbustabad@gmail.com>
2018-09-27 19:07:20 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    SongsWorker.perform_async(params[:songs].path)
+    SongsWorker.perform_async(params[:file].path)
     redirect_to songs_path
   end
 
